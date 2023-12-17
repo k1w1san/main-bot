28bb9425cbb2589d2887d3409f9e5e18feadc6cd
k1w1san <pyankovegor2007@gmail.com>
2023-12-17 21:18:07 +0500

    1.02
    текст

--- bot.py
+++ bot.py
@@ -21,21 +21,10 @@ def say_bye(message):
     bot.send_message(message.chat.id, f"{random.choice(info.bye_list)}, {message.from_user.username}!")
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    bot.send_message(message.chat.id, '''
-        Привет! Я Бот-визитка <Absolute bot>. Я существую для представления различных персонажей.\n
-        - /start - активация бота\n
-        - /help - список существующих команд''')
+    bot.send_message(message.chat.id, info.start_text)
 @bot.message_handler(commands=['help'])
 def help_command(message):
-    bot.send_message(message.chat.id, '''
-        Вот список существующих команд бота:
-        - /name - представление персонажа\n
-        - /age - узнать возраст персонажа\n
-        - /race - узнать расу персонажа\n
-        - /hobby - увидеть список хобби\n
-        - /character_traits - узнать черты характера персонажа\n
-        - /skills - узнать навыки и способности персонажа\n
-        - /photo - увидеть как я выгляжу :)''')
+    bot.send_message(message.chat.id, info.help_text)
 @bot.message_handler(commands=['name'])
 def name_command(message):
     bot.send_message(message.chat.id, info.name_text)
